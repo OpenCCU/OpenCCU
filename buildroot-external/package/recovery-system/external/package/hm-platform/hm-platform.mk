6759ff5e06d09ec0b1ee3ba34b3242552e914743
Jens Maus <mail@jens-maus.de>
2025-09-20 00:37:55 +0200

    integrate coderabbitai nitpicking comments

--- buildroot-external/package/recovery-system/external/package/hm-platform/hm-platform.mk
+++ buildroot-external/package/recovery-system/external/package/hm-platform/hm-platform.mk
@@ -8,6 +8,10 @@
 HM_PLATFORM_VERSION = $(OCCU_VERSION)
 HM_PLATFORM_SITE = $(call github,OpenCCU,occu,$(OCCU_VERSION))
 
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),)
+	$(error hm-platform requires a glibc toolchain (BR2_TOOLCHAIN_USES_GLIBC))
+endif
+
 ifeq ($(BR2_arm),y)
 	HM_PLATFORM_ARCH=arm-gnueabihf-gcc8
 endif
@@ -24,16 +28,18 @@ ifeq ($(BR2_x86_64),y)
 	HM_PLATFORM_ARCH=x86_64-linux-gnu
 endif
 
+HM_PLATFORM_BETA_SUFFIX ?= -Beta
+
 define HM_PLATFORM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis-Beta/bin/ssdpd $(TARGET_DIR)/bin/ssdpd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis-Beta/bin/eq3configcmd $(TARGET_DIR)/bin/eq3configcmd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis-Beta/bin/eq3configd $(TARGET_DIR)/bin/eq3configd
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis-Beta/lib/libeq3config.so $(TARGET_DIR)/lib/libeq3config.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD-Beta/bin/crypttool $(TARGET_DIR)/bin/crypttool
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD-Beta/lib/libLanDeviceUtils.so $(TARGET_DIR)/lib/libLanDeviceUtils.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD-Beta/lib/libUnifiedLanComm.so $(TARGET_DIR)/lib/libUnifiedLanComm.so
-	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD-Beta/lib/libelvutils.so $(TARGET_DIR)/lib/libelvutils.so
-	cp -a $(HM_PLATFORM_PKGDIR)/rootfs-overlay/* $(TARGET_DIR)/
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/ssdpd $(TARGET_DIR)/bin/ssdpd
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/eq3configcmd $(TARGET_DIR)/bin/eq3configcmd
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/bin/eq3configd $(TARGET_DIR)/bin/eq3configd
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/LinuxBasis$(HM_PLATFORM_BETA_SUFFIX)/lib/libeq3config.so $(TARGET_DIR)/lib/libeq3config.so
+	$(INSTALL) -D -m 0755 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/bin/crypttool $(TARGET_DIR)/bin/crypttool
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libLanDeviceUtils.so $(TARGET_DIR)/lib/libLanDeviceUtils.so
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libUnifiedLanComm.so $(TARGET_DIR)/lib/libUnifiedLanComm.so
+	$(INSTALL) -D -m 0644 $(@D)/$(HM_PLATFORM_ARCH)/packages-eQ-3/RFD$(HM_PLATFORM_BETA_SUFFIX)/lib/libelvutils.so $(TARGET_DIR)/lib/libelvutils.so
+	cp -a $(HM_PLATFORM_PKGDIR)/rootfs-overlay/. $(TARGET_DIR)/
 endef
 
 define HM_PLATFORM_INSTALL_INIT_SYSV
