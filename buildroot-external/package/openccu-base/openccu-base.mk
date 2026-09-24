10f40e347c690711cc1f088049627bac311dcd23
Jens Maus <mail@jens-maus.de>
2026-09-24 22:58:53 +0200

    build(base): pin merged component-selection commit
    Use the squash merge of OpenCCU-Base #56 and regenerate its canonical Buildroot git4 archive hash. The merged source tree matches the previously tested PR head.
    
    Signed-off-by: Jens Maus <mail@jens-maus.de>

--- buildroot-external/package/openccu-base/openccu-base.mk
+++ buildroot-external/package/openccu-base/openccu-base.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCCU_BASE_VERSION = 963ab46c42299ac93cde0524159f3efda45d89c2
+OPENCCU_BASE_VERSION = 283d3e5cd3b870683ddfb925f21f8d8d3b56b6ef
 OPENCCU_BASE_COMPAT_VERSION = 3.89.11
 OPENCCU_BASE_SITE = https://github.com/OpenCCU/OpenCCU-Base
 OPENCCU_BASE_SITE_METHOD = git
